237989cccff64e0796355b796a24a09d30e65b7f
vitamaxdh <hellonewtry@gmail.com>
2024-02-10 02:13:49 -0500

    solve: convert array to bin search

---
+++ src/main/kotlin/topic/binarysearch/easy/_108_convert_sorted_arrayto_binary_search_tree.kt
@@ -0,0 +1,19 @@
+package topic.binarysearch.easy
+
+import common.TreeNode
+
+class _108_convert_sorted_arrayto_binary_search_tree {
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        if (nums.isEmpty()) return null
+        fun build(left: Int, right: Int): TreeNode? {
+            if (left > right) return null
+            val mid = (left + right) ushr 1
+            return TreeNode(nums[mid]).apply {
+                this.left = build(left, mid - 1)
+                this.right = build(mid + 1, right)
+            }
+        }
+        return build(0, nums.lastIndex)
+    }
+
+}
---
+++ src/main/kotlin/topic/linkedlist/medium/_109_convert_sorted_listto_binary_search_tree.kt
@@ -0,0 +1,4 @@
+package topic.linkedlist.medium
+
+class _109_convert_sorted_listto_binary_search_tree {
+}
