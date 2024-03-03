30e6260474af21f3fafbf426114573f68b2a272c
vitamaxdh <hellonewtry@gmail.com>
2024-03-03 18:13:23 -0500

    tree-easy: 993 cousin

--- src/main/kotlin/topic/tree/easy/_993_cousinsin_binary_tree.kt
+++ src/main/kotlin/topic/tree/easy/_993_cousinsin_binary_tree.kt
@@ -6,23 +6,24 @@ object _993_cousinsin_binary_tree {
     fun isCousins(root: TreeNode?, x: Int, y: Int): Boolean {
         root!!.printTree()
         var parent: TreeNode? = null
-        var level = 0
+        var level: Int = Int.MIN_VALUE
         val queue = ArrayDeque<Pair<TreeNode, Int>>()
         queue.add(Pair(root, 0))
         while (queue.isNotEmpty()) {
-            val e = queue.removeFirst()
+            val q = queue.removeFirst()
+            val node = q.first
 
-            if (e.first.left?.`val` == x || e.first.right?.`val` == x) {
-                parent = e.first
-                level = e.second
+            if (node.left?.`val` == x || node.right?.`val` == x) {
+                parent = node
+                level = q.second
                 queue.clear()
                 break
             }
-            e.first.left?.let {
-                queue.add(Pair(it, e.second + 1))
+            node.left?.let {
+                queue.add(Pair(it, q.second + 1))
             }
-            e.first.right?.let {
-                queue.add(Pair(it, e.second + 1))
+            node.right?.let {
+                queue.add(Pair(it, q.second + 1))
             }
         }
 
@@ -31,28 +32,30 @@ object _993_cousinsin_binary_tree {
         queue.clear()
         queue.add(Pair(root, 0))
         while (queue.isNotEmpty()) {
-            val e = queue.removeFirst()
+            val q = queue.removeFirst()
+            val node = q.first
 
-            if ((e.first.left?.`val` == y || e.first.right?.`val` == y) && parent != e.first && level == e.second) {
+            if ((node.left?.`val` == y || node.right?.`val` == y) && parent != node && level == q.second) {
                 return true
             }
-            e.first.left?.let {
-                queue.add(Pair(it, e.second + 1))
+            node.left?.let {
+                queue.add(Pair(it, q.second + 1))
             }
-            e.first.right?.let {
-                queue.add(Pair(it, e.second + 1))
+            node.right?.let {
+                queue.add(Pair(it, q.second + 1))
             }
         }
 
-        return false;
+        return false
     }
 }
 
 fun main() {
     with(_993_cousinsin_binary_tree) {
+//        println(isCousins(TreeNode.fromList(1,2,3,4),4,3))
+//        println(isCousins(TreeNode.fromList(1,2,3,null,4,null,5),5,4))
+//        println(isCousins(TreeNode.fromList(1,2,3,null,4),2,3))
+//        println(isCousins(TreeNode.fromList(1,2,3,null,null,null,4,5),1,2))
         println(isCousins(TreeNode.fromList(1,2,3,4),4,3))
-        println(isCousins(TreeNode.fromList(1,2,3,null,4,null,5),5,4))
-        println(isCousins(TreeNode.fromList(1,2,3,null,4),2,3))
-        println(isCousins(TreeNode.fromList(1,2,3,null,null,null,4,5),1,2))
     }
 }
