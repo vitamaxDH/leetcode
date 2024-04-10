19f7dc3078ee8405a413b8acab4cf9b1177756c8
vitamaxdh <hellonewtry@gmail.com>
2024-04-10 18:03:29 -0400

    top 150 med: int to roman

---
+++ src/main/kotlin/top_150/_12_integer_to_roman.java
@@ -0,0 +1,57 @@
+package top_150;
+
+public class _12_integer_to_roman {
+    public String intToRoman(int num) {
+        var sb = new StringBuilder();
+        while (num != 0) {
+            if (num >= 1000) {
+                sb.append('M');
+                num -= 1000;
+            } else if (num >= 900) {
+                sb.append("CM");
+                num -= 900;
+            } else if (num >= 500) {
+                sb.append('D');
+                num -= 500;
+            } else if (num >= 400) {
+                sb.append("CD");
+                num -= 400;
+            } else if (num >= 100) {
+                sb.append('C');
+                num -= 100;
+            } else if (num >= 90) {
+                sb.append("XC");
+                num -= 90;
+            } else if (num >= 50) {
+                sb.append("L");
+                num -= 50;
+            } else if (num >= 40) {
+                sb.append("XL");
+                num -= 40;
+            } else if (num >= 10) {
+                sb.append('X');
+                num -= 10;
+            } else if (num >= 9) {
+                sb.append("IX");
+                num -= 9;
+            } else if (num >= 5) {
+                sb.append('V');
+                num -= 5;
+            } else if (num >= 4) {
+                sb.append("IV");
+                num -= 4;
+            } else if (num >= 1) {
+                sb.append('I');
+                num -= 1;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        var s = new _12_integer_to_roman();
+        System.out.println(s.intToRoman(3));
+        System.out.println(s.intToRoman(58));
+        System.out.println(s.intToRoman(1994));
+    }
+}
