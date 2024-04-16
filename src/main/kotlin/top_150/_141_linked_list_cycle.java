a83aa2b854c00a10b2d293166406b59486f8799d
vitamaxdh <hellonewtry@gmail.com>
2024-04-15 21:45:27 -0400

    top 150 med: 141 linked list cycle with better solution

--- src/main/kotlin/top_150/_141_linked_list_cycle.java
+++ src/main/kotlin/top_150/_141_linked_list_cycle.java
@@ -59,4 +59,14 @@ class ListNode {
         }
         return head;
     }
+
+    public void print() {
+        ListNode current = this;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
 }
