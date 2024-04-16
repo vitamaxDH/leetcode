f1fa5210b723e1ebb8981e9ae00ef36bbf216f52
vitamaxdh <hellonewtry@gmail.com>
2024-04-15 20:44:38 -0400

    top 150 easy: 141 linked list cycle

---
+++ src/main/kotlin/top_150/_141_linked_list_cycle.java
@@ -0,0 +1,62 @@
+package top_150;
+
+
+public class _141_linked_list_cycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        while (head != null) {
+            if (head.val == Integer.MAX_VALUE) {
+                return true;
+            }
+            head.val = Integer.MAX_VALUE;
+            head = head.next;
+        }
+
+        return false;
+    }
+
+    public boolean betterSolution(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        var s = new _141_linked_list_cycle();
+        System.out.println(s.hasCycle(ListNode.createList(3, 2, 0, -4)));
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    public static ListNode createList(int... values) {
+        if (values == null || values.length == 0) return null;
+
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+        return head;
+    }
+}
