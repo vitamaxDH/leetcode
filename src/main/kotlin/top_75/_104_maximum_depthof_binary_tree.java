aa9cce61f0f3a0931fa62c70ba67981cdbd81150
vitamaxdh <hellonewtry@gmail.com>
2024-04-29 21:26:32 -0400

    top 75 easy: 104 maximum depth of bin tree

---
+++ src/main/kotlin/top_75/_104_maximum_depthof_binary_tree.java
@@ -0,0 +1,65 @@
+package top_75;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _104_maximum_depthof_binary_tree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static TreeNode buildTree(Integer... values) {
+        if (values == null || values.length == 0 || values[0] == null) {
+            return null; // return empty tree if no values or first value is null
+        }
+
+        TreeNode root = new TreeNode(values[0]); // first value is the root
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+
+        while (!queue.isEmpty() && i < values.length) {
+            TreeNode current = queue.poll();
+
+            // Assign left child if present
+            if (i < values.length && values[i] != null) {
+                current.left = new TreeNode(values[i]);
+                queue.offer(current.left);
+            }
+            i++;
+
+            // Assign right child if present
+            if (i < values.length && values[i] != null) {
+                current.right = new TreeNode(values[i]);
+                queue.offer(current.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+}
