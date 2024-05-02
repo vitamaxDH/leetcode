c2ec503cd650bb98aa2bebe68ddcbbf9b45a75ac
vitamaxdh <hellonewtry@gmail.com>
2024-05-01 21:22:17 -0400

    top 75 med: 1004 max consecutive ones with solution

---
+++ src/main/kotlin/top_75/_1004_max_consecutive_ones_iii.java
@@ -0,0 +1,30 @@
+package top_75;
+
+import java.util.Arrays;
+
+public class _1004_max_consecutive_ones_iii {
+    public int longestOnes(int[] nums, int k) {
+        int l = 0;
+        int max = 0;
+        int zeroCnt = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] == 0) {
+                zeroCnt++;
+            }
+            if (zeroCnt > k) {
+                if (nums[l] == 0) zeroCnt--;
+                l++;
+            }
+            if (zeroCnt <= k) {
+                max = Math.max(max, r - l + 1);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        var s = new _1004_max_consecutive_ones_iii();
+        System.out.println(s.longestOnes(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 2));
+        System.out.println(s.longestOnes(new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3));
+    }
+}
