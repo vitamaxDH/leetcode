0e53d6f03dc9b890f22d61987d69ee3d5494f571
vitamaxdh <hellonewtry@gmail.com>
2024-08-19 19:02:49 -0400

    sliding-window: longest substring with same letters after replacement (hard) (not solved)

---
+++ src/main/kotlin/designguru/designguru_grokking_the_coding_interview_patterns_for_coding_questions/slidingwindow/LongestSubstringWithSameLettersAfterReplacement.java
@@ -0,0 +1,9 @@
+package designguru.designguru_grokking_the_coding_interview_patterns_for_coding_questions.slidingwindow;
+
+public class LongestSubstringWithSameLettersAfterReplacement {
+    public int findLength(String str, int k) {
+        int maxLength = 0;
+        // TODO: Write your code here
+        return maxLength;
+    }
+}
