d0b6f93a1b7ab5df6e342e6b9a36aa54d3cd48e2
vitamaxdh <hellonewtry@gmail.com>
2025-02-02 17:22:05 -0500

    top 75 easy: 345 reverse vowels of a string

---
+++ src/main/kotlin/top_75_2/_345_reverse_vowels_of_a_string.java
@@ -0,0 +1,39 @@
+package top_75_2;
+
+public class _345_reverse_vowels_of_a_string {
+    public String reverseVowels(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        char[] chars = s.toCharArray();
+        while (l < r) {
+            while (l < r && !isVowel(chars[l])){
+                l++;
+            }
+            while (l < r && !isVowel(chars[r])){
+                r--;
+            }
+            if (l >= r) {
+                break;
+            }
+            int temp = chars[l];
+            chars[l] = chars[r];
+            chars[r] = (char) temp;
+            l++;
+            r--;
+        }
+        return new String(chars);
+    }
+
+    public boolean isVowel(char c) {
+        return switch (c) {
+            case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' -> true;
+            default -> false;
+        };
+    }
+
+    public static void main(String[] args) {
+        _345_reverse_vowels_of_a_string test = new _345_reverse_vowels_of_a_string();
+        System.out.println(test.reverseVowels("IceCreAm"));
+        System.out.println(test.reverseVowels("leetcode"));
+    }
+}
